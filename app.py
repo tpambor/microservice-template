2d4715bda3c7b24fb920a164b5e7bd107d95244d
Tim Pambor <t.pambor@uniandes.edu.co>
2024-08-21 08:20:48 +0000

    Add cloud logging

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from quart import Quart
 from blueprints import BlueprintHealth
+from gcp import setup_cloud_logging
 
 API_PREFIX = "/v1/user"
 
 def create_app():
+    setup_cloud_logging()
+
     app = Quart(__name__)
 
     app.register_blueprint(BlueprintHealth, url_prefix=API_PREFIX)
